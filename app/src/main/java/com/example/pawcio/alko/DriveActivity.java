9607553008423c44d4ed7336774ffbf2ebb732b9
Pawel Karwowski <pkarwowski59@gmail.com>
2019-01-08 22:06:46 +0100

    Pre-final version of AlkoApp

--- app/src/main/java/com/example/pawcio/alko/DriveActivity.java
+++ app/src/main/java/com/example/pawcio/alko/DriveActivity.java
@@ -2,6 +2,7 @@ package com.example.pawcio.alko;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class DriveActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -52,117 +54,134 @@ public class DriveActivity extends AppCompatActivity implements AdapterView.OnIt
                         dialog.cancel();
                     }
                 });
-        if (Text.equals("Polska")) {
-            double legalLimit = 0.2;
-            myImageView.setImageResource(R.drawable.mappoland);
 
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+        if (getIntent().getStringExtra("BAC_Result").equals("9.223372036854776E14")) {
+            AlertDialog.Builder builderValid = new AlertDialog.Builder(DriveActivity.this);
+            builderValid.setTitle("Uwaga!");
+            builderValid.setCancelable(true);
+            builderValid.setMessage("Dane błędnie uzupełnione. Spróbuj jeszcze raz.");
+            AlertDialog alert = builderValid.create();
+            alert.setButton(AlertDialog.BUTTON_NEUTRAL, "Cofnij",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent activityChangeIntent1 = new Intent(DriveActivity.this, MainActivity.class);
+                            startActivity(activityChangeIntent1);
+                        }
+                    });
+            alert.show();
+        } else {
+            if (Text.equals("Polska")) {
+                double legalLimit = 0.2;
+                myImageView.setImageResource(R.drawable.mappoland);
+
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
-        }
 
-        if (Text.equals("Niemcy")) {
-            double legalLimit = 0.5;
-            myImageView.setImageResource(R.drawable.mapgermany);
+            if (Text.equals("Niemcy")) {
+                double legalLimit = 0.5;
+                myImageView.setImageResource(R.drawable.mapgermany);
 
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
-        }
 
-        if (Text.equals("Czechy")) {
-            double legalLimit = 0.0;
-            myImageView.setImageResource(R.drawable.mapczech);
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+            if (Text.equals("Czechy")) {
+                double legalLimit = 0.0;
+                myImageView.setImageResource(R.drawable.mapczech);
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
-        }
 
-        if (Text.equals("Słowacja")) {
-            double legalLimit = 0.0;
-            myImageView.setImageResource(R.drawable.mapslovakia);
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+            if (Text.equals("Słowacja")) {
+                double legalLimit = 0.0;
+                myImageView.setImageResource(R.drawable.mapslovakia);
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
-        }
 
-        if (Text.equals("Ukraina")) {
-            double legalLimit = 0.2;
-            myImageView.setImageResource(R.drawable.mapukraina);
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+            if (Text.equals("Ukraina")) {
+                double legalLimit = 0.2;
+                myImageView.setImageResource(R.drawable.mapukraina);
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
-        }
 
-        if (Text.equals("Białoruś")) {
-            double legalLimit = 0.0;
-            myImageView.setImageResource(R.drawable.mapbialorus);
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+            if (Text.equals("Białoruś")) {
+                double legalLimit = 0.0;
+                myImageView.setImageResource(R.drawable.mapbialorus);
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
-        }
 
-        if (Text.equals("Litwa")) {
-            double legalLimit = 0.4;
-            myImageView.setImageResource(R.drawable.maplithuana);
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+            if (Text.equals("Litwa")) {
+                double legalLimit = 0.4;
+                myImageView.setImageResource(R.drawable.maplithuana);
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
-        }
 
-        if (Text.equals("Rosja")) {
-            double legalLimit = 0.3;
-            myImageView.setImageResource(R.drawable.maprussia);
-            if (resultatDriveActivity >= legalLimit) {
-                builder1.setMessage(levelBAC + "\n" + cantDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
-            } else {
-                builder1.setMessage(levelBAC + "\n" + canDrive);
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+            if (Text.equals("Rosja")) {
+                double legalLimit = 0.3;
+                myImageView.setImageResource(R.drawable.maprussia);
+                if (resultatDriveActivity >= legalLimit) {
+                    builder1.setMessage(levelBAC + "\n" + cantDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else {
+                    builder1.setMessage(levelBAC + "\n" + canDrive);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                }
             }
         }
     }
